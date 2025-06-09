32bd6403025b2ce4e8fec79eafa7f87cbe2e5c1f
Srivignesh Kavle <ramnathkavle@gmail.com>
2025-06-09 11:32:28 -0400

    Testing the webhooks

--- config/conn.php
+++ config/conn.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$db = 'eventjoin';
-$user = 'root';
-$pass = '';
+$db = 'u522900848_capstone';
+$user = 'u522900848_capstone';
+$pass = 'Check_Captone@321';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
