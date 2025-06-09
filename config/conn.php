a673fdd476d467c1c8bde5589c68ab0d21e39633
Srivignesh Kavle <ramnathkavle@gmail.com>
2025-06-09 11:38:09 -0400

    Updated the config file accoring to the local and server

--- config/conn.php
+++ config/conn.php
@@ -1,9 +1,16 @@
 <?php
-$host = 'localhost';
-$db = 'u522900848_capstone';
-$user = 'u522900848_capstone';
-$pass = 'Check_Captone@321';
-
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+    // Local dev
+    $host = 'localhost';
+	$db = 'eventjoin';
+	$user = 'root';
+	$pass = '';
+} else {
+    $host = 'localhost';
+	$db = 'u522900848_capstone';
+	$user = 'u522900848_capstone';
+	$pass = 'Check_Captone@321';
+}
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -11,3 +18,4 @@ try {
     die("DB Connection failed: " . $e->getMessage());
 }
 ?>
+
